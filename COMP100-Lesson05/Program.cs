2234683925e5118ba01bc2970f97bef8c61d27b7
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-02-04 19:42:19 -0500

    basic example of console input and output

--- COMP100-Lesson05/Program.cs
+++ COMP100-Lesson05/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+
 namespace COMP100_Lesson05
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Your Code Here
+            string prompt;
+            string name;
+            int age;
+
+            Console.Write("Please Enter your name: ");
+            prompt = Console.ReadLine();
+            name = prompt;
+
+            Console.Write("Please Enter your age: ");
+            prompt = Console.ReadLine();
+
+            age = Convert.ToInt32(prompt);
+
+
+            Console.WriteLine();
+            Console.WriteLine("+++++++++++++++++++++++++");
+            Console.WriteLine("Your name is: {0}", name);
+            Console.WriteLine();
+            Console.WriteLine("Your age is: {0}", age);
+            Console.WriteLine();
+            Console.WriteLine("+++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
